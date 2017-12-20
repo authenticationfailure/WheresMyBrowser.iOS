e783d116fc938a775d276d2263297a3b10c84f54
AuthenticationFailure <32193068+authenticationfailure@users.noreply.github.com>
2017-12-20 13:24:23 +0000

    Initial commit

---
+++ WheresMyBrowser/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WheresMyBrowser
+//
+//  Created by David Turco on 20/12/2017.
+//  Copyright © 2017 David Turco. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WheresMyBrowserTests/WheresMyBrowserTests.swift
@@ -0,0 +1,36 @@
+//
+//  WheresMyBrowserTests.swift
+//  WheresMyBrowserTests
+//
+//  Created by David Turco on 20/12/2017.
+//  Copyright © 2017 David Turco. All rights reserved.
+//
+
+import XCTest
+@testable import WheresMyBrowser
+
+class WheresMyBrowserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
